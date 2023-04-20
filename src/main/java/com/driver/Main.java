2a355c4710f8a1de0bf13149ea432cad775405ae
Ketan Khanaj <khanajketan@gmail.com>
2023-04-20 15:50:39 +0530

    2nd encapsulation commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,10 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
-        //'member' has private access in 'com.driver.RWOnly'
-//        obj.member = 1;
-//        System.out.print(obj.member);
 
         obj.setMember(1);
         int ans = obj.getMember();
-        System.out.print(ans);
+
 
     }
 }
