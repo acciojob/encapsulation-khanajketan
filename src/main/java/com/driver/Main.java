eb24efc2a914913b46be746b2ce9725ed017e765
Ketan Khanaj <khanajketan@gmail.com>
2023-04-20 15:46:50 +0530

    1st commit encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //'member' has private access in 'com.driver.RWOnly'
+//        obj.member = 1;
+//        System.out.print(obj.member);
+
+        obj.setMember(1);
+        int ans = obj.getMember();
+        System.out.print(ans);
+
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int member;
+
+    public void setMember(int member) {
+        this.member = member;
+    }
+    public int getMember(){
+        return this.member;
+    }
 }
